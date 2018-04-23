98fa70e2a434289000d8ee732f1b58ebb50a0357
Mora-5-Love <644025788@qq.com>
2018-04-23 03:29:53 -0400

    String.cpp add

---
+++ String.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+
+class String
+{
+	public:
+		String(const char* str=nullptr);
+		String(const String &other);
+		~String(void);
+		String & operator = (const String other);
+	private:
+		char *my_data;
+};
+String::String(const char *str)
+{
+	if(nullptr == str){
+		my_data = new char[1];
+		my_data = '\0';
+	}
+	else
+	{
+		auto len = strlen(str);
+		my_data = new char[len+1];
+		strcpy(my_data,str);
+	}
+}
+String::String(const String &other)
+{
+	auto len = strlen(other.my_data);
+	my_data = new char[len+1];
+	strcpy(my_data,other.my_data);
+}
+
+String & String::operator = (const String& other)
+{
+	if(this == &other)
+	{
+		return *this;
+	}
+	
+	delete [] my_data;
+	auto len = strlen(other.my_data);
+	my_data = new char[len+1];
+	strcpy(my_data,other.mydata);
+
+	return *this;
+}
+String::~String(void)
+{
+	delete [] my_data;
+}
+
